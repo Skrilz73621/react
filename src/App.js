32f2f60b271e4f7ccd4f7896e651e2365861e13b
nurs <ailawanlove736@gmail.com>
2024-08-29 02:09:31 +0600

    Сделал дз

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Header(){
+  return(
+    <>
+    <h2>Header component</h2>
+    <Title/>
+    <hr></hr>
+    </>
+  ) 
+}
+
+function Footer(){
+  return(
+    <>
+    <h2>Footer component</h2>
+    <Title/>
+    <hr></hr>
+    </>
+  ) 
+}
+
+
+function Content(){
+  return(
+    <>
+      <p>Content 1</p>
+      <p>Content 2</p>
+      <p>Content 3</p>
+      <Title/>
+      <hr></hr>
+    </>
+  ) 
+}
+
+
+function Title(){
+  return(
+    <h6>little text</h6>
+  ) 
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
